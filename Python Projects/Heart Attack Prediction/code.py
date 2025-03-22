5af3fc9c74128cab0b1dbd6093cbe34b2e026248
Purva Khandekar <purvakhandekar7575@gmail.com>
2025-03-22 16:45:58 -0400

    Update code.py

--- Python Projects/Heart Attack Prediction/code.py
+++ Python Projects/Heart Attack Prediction/code.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import scipy.stats as stats
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+# Link for the dataset: https://www.kaggle.com/datasets/m1relly/heart-attack-prediction
+
+# reading the initial dataset extracted from kaggle into a dataframe called "data"
+data = pd.read_csv('/content/heart_attack_prediction_dataset.csv')
+
+
+# describing the dataframe to understand the nature variables
+data.head()
+data.describe()
